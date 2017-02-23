53ca4fc59cd1f00607363dc38872c597f797d90b
Metin Toksoz-Exley <mt2934a@american.edu>
2017-02-23 15:08:07 -0500

    Update LikelihoodComputation.py

--- LikelihoodComputation.py
+++ LikelihoodComputation.py
@@ -1,20 +1,20 @@
 #Metin Toksoz-Exley GEPHI
 
 #Grab a random pathway 
-import csv
-import random 
-with open('SelectedPathways.csv', 'rt', 'wt') as file:
-    contents = csv.reader(file)
-    [x for x in contents]
+import csv 
+import random  
 
+def random_row(file): 
+    line = next(file) 
+    for num, row in enumerate(file): 
+      if random.randrange(num + 2): continue 
+      line = row 
+    return line 
 
-def random_row(file):
-    line = next(file)
-    for num, row in enumerate(file):
-      if random.randrange(num + 2): continue
-      line = row
-    return line
-
+with open('SelectedPathways.csv', 'rt', 'wt') as file: 
+    contents = csv.reader(file) 
+    [x for x in contents] 
+    random_row(file)
 random_row(file)
     #assuming I don't need to limit it to the 633 actual rows because it has imported all data so 
     #random should stay within that constraint
