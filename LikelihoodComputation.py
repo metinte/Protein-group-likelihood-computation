92d0bac20320382902d3c97a288bacf6ae13006a
Metin Toksoz-Exley <mt2934a@american.edu>
2017-03-03 09:21:17 -0500

    Update LikelihoodComputation.py

--- LikelihoodComputation.py
+++ LikelihoodComputation.py
@@ -4,18 +4,19 @@
 import csv 
 import random  
 
-def random_row(file): 
-    line = next(file) 
-    for num, row in enumerate(file): 
-      if random.randrange(num + 2): continue 
-      line = row 
-    return line 
 
-with open('SelectedPathways.csv', 'rt', 'wt') as file: 
+with open('SelectedPathways.csv', 'rt') as file: 
     contents = csv.reader(file) 
-    [x for x in contents] 
-    random_row(file)
-random_row(file)
+    listpathways = [x for x in contents] 
+print listpathways
+print contents
+#random_row=random.randrange(len(listpathways))
+#listpathways[random_row]
+
+
+#REWRITE THIS SO THAT IT WORKS WITH LIST PATHWAYS THINGY 
+
+    
     #assuming I don't need to limit it to the 633 actual rows because it has imported all data so 
     #random should stay within that constraint
 
